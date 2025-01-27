2ea86c178b4b51acc3f9b5d0a1832ad0c38f4e79
Joe <joediaz88@hotmail.com>
2025-01-27 16:47:23 +0000

    ejercicio resuelto

--- src/models.py
+++ src/models.py
@@ -13,34 +13,34 @@ db = SQLAlchemy(app)
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True,unique=True,index=True,nullable=False)
-    username = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), nullable=False,unique=True)
     firstname = db.Column(db.String(50))
     lastname = db.Column(db.String(50))
-    email = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False,unique=True)
 
 class Post(db.Model):
     __tablename__ = 'post'
-    id = db.Column(db.Integer, unique=True, primary_key=True)
+    id = db.Column(db.Integer,unique=True, primary_key=True,index=True)
     user_id = db.Column(db.Integer, ForeignKey('user.id'),nullable=False)
 
 class Comment(db.Model):
     __tablename__ = 'comment'
-    id = db.Column(db.Integer,primary_key=True)
-    comment_text = db.Column(db.String(500), nullable=True)
+    id = db.Column(db.Integer,primary_key=True,unique=True)
+    comment_text = db.Column(db.String(500),nullable=False)
     author_id = db.Column(db.Integer,ForeignKey('user.id'),nullable=False)
     post_id = db.Column(db.Integer,ForeignKey('post.id'),nullable=False)
 
 class Media(db.Model):
     __tablename__ = 'media'
-    id = db.Column(db.Integer,primary_key=True,nullable=False)
-    type = db.Column(db.String(50),nullable=True)
-    url = db.Column(db.String(100),nullable=True)
+    id = db.Column(db.Integer,primary_key=True,unique=True,nullable=False)
+    type = db.Column(db.String(50),nullable=False)
+    url = db.Column(db.String(100),nullable=False)
     post_id = db.Column(db.Integer,ForeignKey('post.id'),nullable=False)
 
 class Follower(db.Model):
     __tablename__ = 'follower'
-    user_from_id = db.Column(db.Integer,ForeignKey('post.id'),primary_key=True)
-    user_to_id = db.Column(db.Integer,ForeignKey('post.id'),primary_key=True)
+    user_from_id = db.Column(db.Integer,ForeignKey('user.id'),primary_key=True)
+    user_to_id = db.Column(db.Integer,ForeignKey('user.id'),primary_key=True)
 
 
     def to_dict(self):
