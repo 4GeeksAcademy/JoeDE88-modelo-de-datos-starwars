3278a9c10b237746ec3ec831584f77d11e40d3e0
Joe <joediaz88@hotmail.com>
2025-01-27 20:29:17 +0000

    creado modelo de base de datos de Star Wars

--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import enum
 from sqlalchemy import ForeignKey, Integer, String
 from eralchemy2 import render_er
 from flask import Flask
@@ -10,37 +11,56 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
 app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 
+
+
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True,unique=True,index=True,nullable=False)
     username = db.Column(db.String(50), nullable=False,unique=True)
-    firstname = db.Column(db.String(50))
-    lastname = db.Column(db.String(50))
+    firstname = db.Column(db.String(50),nullable=False)
+    lastname = db.Column(db.String(50),nullable=False)
     email = db.Column(db.String(50), nullable=False,unique=True)
 
-class Post(db.Model):
-    __tablename__ = 'post'
+class FavoriteTypeEnum(enum.Enum):
+    Planets = "planets"
+    People = "people"
+    Films = "films"
+
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
     id = db.Column(db.Integer,unique=True, primary_key=True,index=True)
     user_id = db.Column(db.Integer, ForeignKey('user.id'),nullable=False)
+    external_id = db.Column(db.Integer,nullable=False)
+    name = db.Column(db.String(50), unique=True,nullable=False)
+    type = db.Column(db.Enum(FavoriteTypeEnum),nullable=False)
 
-class Comment(db.Model):
-    __tablename__ = 'comment'
+class Films(db.Model):
+    __tablename__ = 'films'
     id = db.Column(db.Integer,primary_key=True,unique=True)
-    comment_text = db.Column(db.String(500),nullable=False)
-    author_id = db.Column(db.Integer,ForeignKey('user.id'),nullable=False)
-    post_id = db.Column(db.Integer,ForeignKey('post.id'),nullable=False)
+    title = db.Column(db.String(50),nullable=False,unique=True)
+    episode = db.Column(db.Integer,nullable=False,unique=True)
+    release_date = db.Column(db.Integer,nullable=False)
+    director = db.Column(db.String(50),nullable=False)
+    producer = db.Column(db.String(50),nullable=False)
 
-class Media(db.Model):
-    __tablename__ = 'media'
+class Planets(db.Model):
+    __tablename__ = 'planets'
     id = db.Column(db.Integer,primary_key=True,unique=True,nullable=False)
-    type = db.Column(db.String(50),nullable=False)
-    url = db.Column(db.String(100),nullable=False)
-    post_id = db.Column(db.Integer,ForeignKey('post.id'),nullable=False)
+    name = db.Column(db.String(50),nullable=False,unique=True)
+    population = db.Column(db.Integer,nullable=False)
+    climate = db.Column(db.String(50),nullable=False)
+    diameter = db.Column(db.String(50),nullable=False)
+    gravity = db.Column(db.Integer,nullable=False)
 
-class Follower(db.Model):
-    __tablename__ = 'follower'
-    user_from_id = db.Column(db.Integer,ForeignKey('user.id'),primary_key=True)
-    user_to_id = db.Column(db.Integer,ForeignKey('user.id'),primary_key=True)
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer,primary_key=True,unique=True,nullable=False)
+    name = db.Column(db.String(50),unique=True,nullable=False)
+    species = db.Column(db.String(50),nullable=False)
+    skin_color = db.Column(db.String(50),nullable=False)
+    hair_color = db.Column(db.String(50),nullable=False)
+    height = db.Column(db.Integer,nullable=False)
+    homeworld = db.Column(db.String,ForeignKey('planets.id'),nullable=False)
 
 
     def to_dict(self):
